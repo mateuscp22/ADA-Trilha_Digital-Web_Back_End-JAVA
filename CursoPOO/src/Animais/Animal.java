53d16a5ae7371f127fbdd2ea33c27eb2706e4dd5
Mateus CP <mateuscandicocp@gmail.com>
2023-11-15 12:54:54 -0300

    Polimorfismo + Classes Abstratas

--- CursoPOO/src/Animais/Animal.java
+++ CursoPOO/src/Animais/Animal.java
@@ -1,6 +1,6 @@
 package Animais;
 
-public class Animal {
+public abstract class Animal {
     protected String nome;
     protected String cor;
     protected int altura;
@@ -22,6 +22,46 @@ public class Animal {
 
     // Métodos
 
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCor() {
+        return this.cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getAltura() {
+        return this.altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public String getEstadoDeEspirito() {
+        return this.estadoDeEspirito;
+    }
+
+    public void setEstadoDeEspirito(String estadoDeEspirito) {
+        this.estadoDeEspirito = estadoDeEspirito;
+    }
+
     protected void comer() {
 
     }
@@ -30,8 +70,6 @@ public class Animal {
 
     }
 
-    public void soar() {
-        System.out.println(" Cri cri");
-    }
+    public abstract void soar();
 
 }
--- CursoPOO/src/Main.java
+++ CursoPOO/src/Main.java
@@ -1,18 +1,14 @@
+import Animais.Animal;
 import Animais.Cachorro;
 import Animais.Gato;
 import Animais.Passaro;
+import Lojas.Petshop;
 
 public class Main {
 
     public static void main(String[] args) {
-        Cachorro cachorro1 = new Cachorro("Spike", "Branco", 60, 20.5, 30, "nada");
-
-        Gato gato1 = new Gato("Felix", null, 0, 0, null);
-
-        Passaro passaro1 = new Passaro("Fajola", "Preto", 50, 10, "Cansado");
-
-        cachorro1.soar();
-        gato1.soar();
-        passaro1.soar();
+        Gato gato1 = new Gato("Felix", "Preto", 10, 5, "Bem");
+        Animal gato2 = new Gato("Felix", "Preto", 10, 5, "Bem");
+        Animal animal4 = new Passaro("Felix", "Preto", 10, 5, "Bem");
     }
 }
